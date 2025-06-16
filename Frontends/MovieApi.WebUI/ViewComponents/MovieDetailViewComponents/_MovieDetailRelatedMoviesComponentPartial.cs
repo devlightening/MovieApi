0531f9c3acefa950869d2e3e8582e002944809c1
Halil Yildirim <ce.hybusiness@gmail.com>
2025-06-16 14:31:44 +0300

    Yapay Zeka ve Veri Analizi ile Film Önerisi Hazırlığı.

---
+++ Frontends/MovieApi.WebUI/ViewComponents/MovieDetailViewComponents/_MovieDetailRelatedMoviesComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.MovieDetailViewComponents
+{
+    public class _MovieDetailRelatedMoviesComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // Here you would typically fetch related movies from a service or database
+            // For now, we will return an empty view
+            return View();
+        }
+    }
+}
